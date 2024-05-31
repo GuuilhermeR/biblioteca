da2266ec94ac3c9db7f046b67653f070ea02f1af
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-05-31 20:25:13 -0300

    Fazendo insert e select e melhoria no código

--- app.py
+++ app.py
@@ -1,38 +1,32 @@
-
-import os, pyodbc
-
+import pyodbc
 from database.database import DB
 
-# https://www.bosontreinamentos.com.br/programacao-em-python/como-acessar-bancos-de-dados-sql-server-usando-python/
-
 def exibir_retorno_select(cursor):
-    cursor.execute('SELECT name, city FROM Person')
-        
+    sql_select = 'SELECT name, city FROM Person'
+    cursor.execute(sql_select)
     for row in cursor:
         print(f'{row.name} - {row.city}')
-    
-        
-def inserir_regitro(cursor):
+
+def inserir_registro(cursor):
+    sql_insert = "INSERT INTO Person (name, bihDate, address, city, cellphone) VALUES (?, ?, ?, ?, ?)"
     pessoa = {
-            "name": "Daniele",
-            "birthDate": "1999-03-14",
-            "address": "Rua Comandante Abdon Senna, 42.",
-            "city": "Joinville, SC",
-            "cellphone": "47999999999"
-            }
-    sql_insert = f"INSERT INTO Person (name, birthDate, address, city, cellphone) VALUES (?, ?, ?, ?, ?)"
-    
+        "name": "Daniele",
+        "birthDate": "1999-03-14",
+        "address": "Rua Comandante Abdon Senna, 42.",
+        "city": "Joinville, SC",
+        "cellphone": "47999999999"
+    }
     cursor.execute(sql_insert, pessoa['name'], pessoa['birthDate'], pessoa['address'], pessoa['city'], pessoa['cellphone'])
     conn.commit()
-    
 
 if __name__ == "__main__":
-        
-    conn = DB().get_connection()
-
-    cursor = conn.cursor()
-    exibir_retorno_select(cursor)
-    inserir_regitro(cursor)
-    exibir_retorno_select(cursor)
-    conn.close()
-    
+    try:
+        with DB().get_connection() as conn:
+            with conn.cursor() as cursor:
+                exibir_retorno_select(cursor)
+                inserir_registro(cursor)
+                exibir_retorno_select(cursor)
+    except pyodbc.Error as e:
+        print(f"Erro de SQL: {e}")
+    except Exception as e:
+        print(f"Erro: {e}")
